c229c5a7ca89dbb3b34186c55e9449a561f76713
cih <angel08012000@gmail.com>
2022-11-05 23:19:27 +0800

    feat: register API 收到參數即回傳
    收到參數先回傳，真正執行完再通知

--- models/bridge.py
+++ models/bridge.py
@@ -5,12 +5,23 @@ Created on Tue Oct 18 19:29:18 2022
 
 @author: cihcih
 """
-import redis
+import requests
 from models import crawler, database
 
 def register(req):
     temp = crawler.get_commit_history(req)
     res = database.get_dbformat_data(temp)
+    mes = "failed"
     if res["status"] == "getting completed":
         res = database.append_to_db(req)
-    return res
+        if res["status"] == "indexing completed":
+            mes = "completed"
+    
+    r = requests.get(f"http://192.168.100.44:10050/commitment/status?message={mes}")
+
+    # r.status_code 伺服器回應的狀態碼
+    # requests.codes.ok 成功的狀態碼，也就是 200
+    if r.status_code==requests.codes.ok and r.text=="ok":
+        print("通知成功！")
+    else:
+        print("通知失敗！")
--- views/bridge_api.py
+++ views/bridge_api.py
@@ -9,14 +9,20 @@ Created on Tue Oct 11 00:43:51 2022
 from models import bridge
 
 from flask import Blueprint, Flask, request, jsonify
-import requests
+from time import timezone
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
 bridge_api=Blueprint('bridge_api', __name__)
 
 @bridge_api.route("/register", methods=['POST'])
 def get_commit_and_create_api():
-    req = request.get_json()
-    res = bridge.register(req)
-    
-    return jsonify(res)
+    try:
+        req = request.get_json()
+        scheduler = BackgroundScheduler(timezone='Asia/Taipei')
+        scheduler.add_job(bridge.register, args=[req])
+        scheduler.start()
+        return jsonify({"status": "recieve completed"})
+        
+    except:
+        return jsonify({"status": "recieve failed"})
